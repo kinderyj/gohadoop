a174b864bd14dd14f7ee4c75fb0c1fd4a6af510f
Matt Wang <kinder_yj@hotmail.com>
2020-06-18 16:43:10 +0800

    Create emr.go

---
+++ hadoop_yarn/examples/dist_shell/emr.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+    "log"
+    "os"
+
+    "github.com/hortonworks/gohadoop/hadoop_yarn"
+    yarn_conf "github.com/hortonworks/gohadoop/hadoop_yarn/conf"
+    "github.com/hortonworks/gohadoop/hadoop_yarn/yarn_client"
+)
+
+const (
+    GOPATH               = "GOPATH"
+    APPLICATIONMASTER_GO = "APPLICATIONMASTER_GO"
+)
+
+func main() {
+    goPath := os.Getenv(GOPATH)
+    applicationMasterLocation := os.Getenv(APPLICATIONMASTER_GO)
+    if len(applicationMasterLocation) == 0 {
+        applicationMasterLocation = goPath + "/src/github.com/hortonworks/gohadoop/hadoop_yarn/examples/dist_shell/applicationmaster.go"
+    }
+
+    // Create YarnConfiguration
+    conf, _ := yarn_conf.NewYarnConfiguration()
+
+    // Create YarnClient
+    yarnClient, _ := yarn_client.CreateYarnClient(conf)
+
+    // Create new application to get ApplicationSubmissionContext
+    //_, asc, _ := yarnClient.CreateNewApplication()
+
+    // Get nodes states
+    yarnClient.GetNodesState()
+
+
+    // Get GetApplicationReport
+    applicationIdProto := hadoop_yarn.ApplicationIdProto{}
+    id := int32(85)
+    applicationIdProto.Id = &id
+    clusterTimestamp := int64(1587465966658)
+    applicationIdProto.ClusterTimestamp = &clusterTimestamp
+    aplication, err := yarnClient.GetApplicationReport(&applicationIdProto)
+    if err != nil {
+        log.Println("the err is ", err)
+    } else {
+        log.Println("the aplication is", aplication)
+    }
+
+}
